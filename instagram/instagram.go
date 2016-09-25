9518ae47a4695da85541370fb75555b7bb5be6e8
Abhi Agarwal <me@abhiagarwal.com>
2016-09-25 16:19:35 -0400

    Fixing comments

--- instagram/instagram.go
+++ instagram/instagram.go
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package heroku provides constants for using OAuth2 to access Heroku.
+// Package instagram provides constants for using OAuth2 to access Instagram.
 package instagram // import "github.com/news-ai/oauth2/instagram"
 
 import (
 	"golang.org/x/oauth2"
 )
 
-// Endpoint is Heroku's OAuth 2.0 endpoint.
+// Endpoint is Instagram's OAuth 2.0 endpoint.
 var Endpoint = oauth2.Endpoint{
 	AuthURL:  "https://api.instagram.com/oauth/authorize",
 	TokenURL: "https://api.instagram.com/oauth/access_token",
